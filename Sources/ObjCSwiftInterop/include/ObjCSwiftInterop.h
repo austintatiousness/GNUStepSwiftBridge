84ce5aa5983ba402d643d7db971cfa685bfc0a2f
Austin <79065594+austintatiousness@users.noreply.github.com>
2023-08-06 11:25:08 -0500

    Update ObjCSwiftInterop.h

--- Sources/ObjCSwiftInterop/include/ObjCSwiftInterop.h
+++ Sources/ObjCSwiftInterop/include/ObjCSwiftInterop.h
@@ -12,7 +12,21 @@
 
 #import "../../../Headers/objc/objc-visibility.h"
 
-typedef unsigned long NSUInteger;
+typedef uintptr_t NSUInteger;
+
+typedef struct _NSRect NSRect;
+struct _NSRect {
+double x;
+double y;
+double width;
+double height;
+};
+
+typedef struct _NSPoint NSPoint;
+struct _NSPoint {
+double x;
+double y;
+};
 
 id forSwift_objcSendMessage(id ID, SEL cmd);
 id forSwift_objcSendMessage1(id ID, SEL cmd, void const *arg1);
@@ -20,3 +34,7 @@ id forSwift_objcSendMessageCString(id ID, SEL cmd, const char* bytes);
 id forSwift_objcSendMessage2(id ID, SEL cmd, void const *arg1, void const *arg2);
 id forSwift_objcSendMessage3(id ID, SEL cmd, void const *arg1, void const *arg2, void const *arg3);
 id forSwift_objcSendMessage4(id ID, SEL cmd, void const *arg1, void const *arg2, void const *arg3, void const *arg4);
+
+id forSwift_objcSendMessage4Floats(id ID, SEL cmd, float arg1, float arg2, float arg3, float arg4);
+id initWithContentRect_styleMask_backing_defer(id ID, SEL cmd, NSRect arg1, NSUInteger arg2, NSUInteger arg3, char arg4);
+id forSwift_objcSendMessage1NSRect(id ID, SEL cmd, NSRect arg1);
