d6a297930fc31d03a0ce734dc3ae703954b8ba88
Austin <79065594+austintatiousness@users.noreply.github.com>
2023-08-10 19:49:54 -0500

    Update AppKit.swift

--- Sources/AppKit/AppKit.swift
+++ Sources/AppKit/AppKit.swift
@@ -11,7 +11,6 @@ import ObjCSwiftInterop
 import ObjectiveC
 #endif
 
-public typealias CGRect = ObjCSwiftInterop.CGRect
 
 //NSApplication
 open class NSApplication: GNUStepNSObjectWrapper {
@@ -30,15 +29,12 @@ open class NSApplicationDelegate: GNUStepNSObjectWrapper {
 	public override var _nsclassName: String {
 		return "NSApplicationDelegateForSwift"
 	}
-	
-	
-	
+
 	static var didFinishLaunchingIMP: @convention(block) (UnsafeMutablePointer<objc_object>?,SEL,id?) -> (UInt8) = { first, second, third in
 		let SELF: NSApplicationDelegate? = smart_swift_lookupIvarWithType(_nsobjptr: first, name: "___swiftPtr")
 		SELF?.applicationDidFinishLaunching(notification: nil)
 		return 0
 	}
-	
 
 	open func applicationDidFinishLaunching(notification: Any?) {
 		
@@ -49,14 +45,11 @@ open class NSApplicationDelegate: GNUStepNSObjectWrapper {
 		let imp = imp_implementationWithBlock(unsafeBitCast(didFinishLaunchingIMP, to: id.self))
 		class_addMethod(ptr, sel_registerName("applicationDidFinishLaunching:"),imp, types)
 	})
-	var string: String = "THIS is THAT"
+
 	public override init() {
 		_ = NSApplicationDelegate._objcClass
 
 		super.init()
-		
-		
-		self.string = "THESE ARE THOSE"
 
 		var nclass = objc_getClass("NSApplicationDelegateForSwift")
 		
@@ -123,6 +116,12 @@ open class UIView: NSView {
 		smart_swift_setIvar(_nsobjptr: self._nsobjptr, name: "___swiftPtr", value: cast)
 
 	}
+	
+	public override init(nsobjptr: UnsafeMutablePointer<objc_object>?) {
+		super.init()
+		self._nsobjptr = nsobjptr
+		var initalizedObject = forSwift_objcSendMessage(&nsobjptr!.pointee, sel_registerName("retain"))
+	}
 
 	
 	
@@ -144,7 +143,56 @@ open class UIView: NSView {
 
 }
 
-///https://gnustep.github.io/resources/OpenStepSpec/ApplicationKit/Classes/NSColor.html
+///https://gnustep.github.io/resources/documentation/Developer/Gui/Reference/NSFont.html#method$NSImage+imageNamed$
+public class NSFont: GNUStepNSObjectWrapper {
+	
+	public override var _nsclassName: String {
+		return "NSFont"
+	}
+	
+	public override init() {
+		super.init()
+	}
+	
+	public init?(name: String, size: Float) {
+		super.init()
+		var nsColorClass = objc_getClass("NSFont")
+		var size = size
+		var string = NSString(string: name)
+		var imp:  (@convention(c) (id, SEL, id, Float) -> (id?))? = objc_smart_getIMP(id: &nsColorClass!.pointee, selector: "fontWithName:size:")
+		if var rtn = imp?(&nsColorClass!.pointee, sel_getUid("fontWithName:size:"), &string._nsobjptr!.pointee, size) {
+			self._nsobjptr = rtn
+			print("FOUND FONT")
+			var initalizedObject = forSwift_objcSendMessage(&rtn.pointee, sel_registerName("retain"))
+		} else {
+			print("FONT NOT FOUND")
+			return nil
+		}
+	}
+	
+	public static func boldSystemFontOfSize(size: Double) -> NSFont {
+		var nsColorClass = objc_getClass("NSFont")
+		var size = size
+
+		var imp:  (@convention(c) (id, SEL, Double) -> (id?))? = objc_smart_getIMP(id: &nsColorClass!.pointee, selector: "boldSystemFontOfSize:")
+		if var rtn = imp?(&nsColorClass!.pointee, sel_getUid("boldSystemFontOfSize:"), size) {
+			print("FOUND boldSystemFontOfSize")
+			
+			var initalizedObject = forSwift_objcSendMessage(&rtn.pointee, sel_registerName("retain"))
+			return NSFont(nsobjptr: rtn)
+
+		} else {
+			return NSFont()
+		}
+	}
+	
+	public override init(nsobjptr: UnsafeMutablePointer<objc_object>?) {
+		super.init()
+		self._nsobjptr = nsobjptr
+		var initalizedObject = forSwift_objcSendMessage(&nsobjptr!.pointee, sel_registerName("retain"))
+	}
+}
+
 public class NSColor: GNUStepNSObjectWrapper {
 
 	public override var _nsclassName: String {
@@ -172,52 +220,95 @@ public class NSColor: GNUStepNSObjectWrapper {
 		//colorWithCalibratedRed
 	}
 		
+	public static var clear: NSColor {
+		return .init(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.0)
+	}
 	
+	public static var blue: NSColor {
+		return .init(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+	}
+	
+	public static var black: NSColor {
+		return .init(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+	}
+	
+	public static var white: NSColor {
+		return .init(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+	}
+	
+	
+	public static var grey: NSColor {
+		return .init(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+	}
+	
+	
+	public static var darkPurple: NSColor {
+		return NSColor.init(red: 0.1422559619, green: 0.05977959186, blue: 0.2294596732, alpha: 1)
+	}
+	
+	public static var purple: NSColor {
+		let c =    NSColor.init(red: 0.2726541758, green: 0.1163508371, blue: 0.435738951, alpha: 1)
+		return c
+	}
+	
+	
+	
+	public override init(nsobjptr: UnsafeMutablePointer<objc_object>?) {
+		super.init()
+		self._nsobjptr = nsobjptr
+		var initalizedObject = forSwift_objcSendMessage(&nsobjptr!.pointee, sel_registerName("retain"))
+	}
 }
 
 
 
-public class NSString: GNUStepNSObjectWrapper {
 
 
+
+open class NSImage: GNUStepNSObjectWrapper {
 	public override var _nsclassName: String {
-		return "NSString"
-	}
-
-	public init(string: String) {
-		super.init()
-		var nsStringClass =  objc_getClass(_nsclassName)
-		var cString = string
-
-		var initalizedObject = forSwift_objcSendMessageCString(&nsStringClass!.pointee, sel_registerName("stringWithCString:"), string )
-		initalizedObject = forSwift_objcSendMessage(&initalizedObject!.pointee, sel_registerName("retain"))
-		self._nsobjptr = initalizedObject
-
+		return "NSImage"
 	}
 	
-	public var string: String {
-		get {
-			if var chars = forSwift_objcSendMessage_ReturnCString(&self._nsobjptr!.pointee, sel_registerName("cString") ) {
-				var v: String = String(cString: chars)
-				return v
-			}
-			return ""
+	public static func image(named: String) -> NSImage? {
+		print("finding image named \(named)")
+		var nsColorClass = objc_getClass("NSImage")
+		var nsColorClassAsClass =  object_getClass(objc_getClass("NSImage"))
+
+		var string = NSString(string: named)
+		var imp:  (@convention(c) (id, SEL, id) -> (id?))? = objc_smart_getIMP(id: &nsColorClass!.pointee, selector: "imageNamed:")
+		print(imp)
+		if var rtn = imp?(&nsColorClass!.pointee, sel_getUid("imageNamed:"), &string._nsobjptr!.pointee) {
+			
+			var initalizedObject = forSwift_objcSendMessage(&rtn.pointee, sel_registerName("retain"))
+			let v = NSImage(nsobjptr: &rtn.pointee)
+			print("image is named \(v.name)")
+			return v
 		}
+		return nil
 	}
 	
 	public override init(nsobjptr: UnsafeMutablePointer<objc_object>?) {
 		super.init()
+		self._nsobjptr = nsobjptr
 		var initalizedObject = forSwift_objcSendMessage(&nsobjptr!.pointee, sel_registerName("retain"))
 	}
 	
-//	deinit {
-//		if var ptr = self._nsobjptr {
-//			_ = forSwift_objcSendMessage(&ptr.pointee, sel_registerName("release"))
-//		}
-//	}
-//	
-	
-	
+	public var name: String {
+		get {
+			print("getting name")
+			if var ptr = self._nsobjptr {
+				var imp: (@convention(c) (id, SEL) -> (id))? = objc_smart_getIMP(object: self, selector: "name")
+				if let rtn = imp?(&ptr.pointee, sel_getUid("name")) {
+					if let rtn = objc_convertToSwift_NSObject(value: rtn) as? NSString {
+						return rtn.string
+					}
+				}
+			}
+			
+			return ""
+		}
+	}
 }
 
 
@@ -245,21 +336,17 @@ open class NSView: GNUStepNSObjectWrapper {
 	
 	public var frame: CGRect {
 		get {
-//			var imp:  (@convention(c) () -> (CGRect))? = objc_smart_getIMP_stret(object: self, selector: "frame")
-//			var rtn = imp!()
-
-			
-			//print("value \(imp?())")
-			if let x: CGRect = objc_smart_sendMessage(object: self, selector: "frame") {
-				print("SUCCESS getting frame")
-				return x
+			var imp:  (@convention(c) (id, SEL) -> (CGRect))? = objc_smart_getIMP(object: self, selector: "frame")
+			if let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("frame")) {
+				return rtn
 			}
-			print("NO SUCCESS getting frame")
 			return .init(x: 0, y: 0, width: 0, height: 0)
 		}
+		
 		set {
 			guard let selfPtr = self._nsobjptr else {return}
-			let _: Any? = objc_smart_sendMessage(object: self, selector: "setFrame:", value1: newValue)
+			var imp:  (@convention(c) (id, SEL, CGRect) -> (Void))? = objc_smart_getIMP(object: self, selector: "setFrame:")
+			let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("setFrame:"), newValue)
 		}
 	}
 	
@@ -352,8 +439,6 @@ public class NSWindow: GNUStepNSObjectWrapper {
 			_ = forSwift_objcSendMessage1(&ptr.pointee, sel_registerName("_rebuildCoordinates"), &ptr.pointee)
 			
 			//initalizedObject = forSwift_objcSendMessage(&initalizedObject!.pointee, sel_getUid("_rebuildCoordinates"))
-			
-			
 		}
 	}
 
@@ -379,7 +464,79 @@ public class NSControl: NSView {
 
 		_ = forSwift_objcSendMessage1(&ptr.pointee, sel_registerName("setEnabled:"), &bool)
 	}
+	
+	public var stringValue: String {
+		get {
+			
+			var imp:  (@convention(c) (id, SEL) -> (id))? = objc_smart_getIMP(object: self, selector: "stringValue")
+			if let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("stringValue")) {
+				if let rtn = objc_convertToSwift_NSObject(value: rtn) as? NSString {
+					return rtn.string
+				}
+			}
+			return ""
+		}
+		set {
+//			guard let selfPtr = self._nsobjptr else {return}
+//			let _: Any? = objc_smart_sendMessage(object: self, selector: "setStringValue:", value1: newValue)
+//			
+			guard var selfPtr = self._nsobjptr else {return}
+			let ns = NSString(string: newValue)
+			guard var stringPTR = ns._nsobjptr else {return}
+			var imp:  (@convention(c) (id, SEL, id) -> (Void))? = objc_smart_getIMP(object: self, selector: "setStringValue:")
+			let rtn = imp?(&selfPtr.pointee, sel_getUid("setStringValue:"), &stringPTR.pointee)
+		}
+	}
 
+}
+
+public class NSImageView: NSView {
+	
+
+	
+	public override var _nsclassName: String {
+		return "NSImageView"
+	}
+
+
+	
+	lazy var newWindow = NSWindow()
+	public lazy var onAction: ((NSButton) -> (Void))? = { first in
+		
+	}
+	
+	public override init() {
+		
+		super.init()
+	
+		var rect = ObjCSwiftInterop.CGRect(x: 0, y: 0, width: 200, height: 22) //(x: Double(50), y: Double(50), width:  Double(50), height: Double(50))
+		let  nsWindowClass =  objc_getClass("NSImageView")
+		var allocatedObject = forSwift_objcSendMessage(&nsWindowClass!.pointee, sel_registerName("alloc"))
+
+		allocatedObject = forSwift_objcSendMessage1NSRect(&allocatedObject!.pointee, sel_registerName("initWithFrame:"), rect)
+		_ =  forSwift_objcSendMessage(&allocatedObject!.pointee, sel_registerName("retain"))
+
+		self._nsobjptr = allocatedObject
+
+	}
+
+
+	
+	
+	public func setImage(_ image: NSImage) {
+		print("Setting Image 1")
+		guard var ptr = image._nsobjptr else {print("Setting Image image ptr == nil"); return}
+		guard var selfPtr = self._nsobjptr else {print("Setting Image 1.2"); return}
+		//if var ptr = image._nsobjptr, var selfPtr = self._nsobjptr {
+			print("Setting Image 2")
+			_ = forSwift_objcSendMessage1ID(&selfPtr.pointee, sel_registerName("setImage:"), ptr)
+		//}
+	}
+
+	
+	
+	
+ 
 }
 
 public class NSButton: NSControl {
@@ -402,9 +559,7 @@ public class NSButton: NSControl {
 	
 	lazy var newWindow = NSWindow()
 	public lazy var onAction: ((NSButton) -> (Void))? = { first in
-		self.newWindow.orderFront(sender: self)
-		self.newWindow.setBackgroundColor(NSColor.init(red: 0, green: 0.5, blue: 0.5, alpha: 1.0))
-		self.setTitle(NSString(string: "New Title"))
+		
 	}
 	
 	public override init() {
@@ -449,6 +604,13 @@ public class NSButton: NSControl {
 		
 		}
 	}
+	
+	public func setImage(_ image: NSImage) {
+		if var ptr = image._nsobjptr, var selfPtr = self._nsobjptr {
+			_ = forSwift_objcSendMessage1ID(&selfPtr.pointee, sel_registerName("setImage:"), ptr)
+		
+		}
+	}
 
 	public func setEditable(_ flag: ObjCBool) {
 		guard var ptr = self._nsobjptr else {return}
@@ -461,7 +623,8 @@ public class NSButton: NSControl {
  
 }
 
-public class NSLabel: NSControl {
+
+public class NSTextField: NSControl {
 	
 	public override var _nsclassName: String {
 		return "NSTextField"
@@ -492,6 +655,24 @@ public class NSLabel: NSControl {
 		
 		}
 	}
+	
+	public var font: NSFont? {
+		get {
+			var imp:  (@convention(c) (id, SEL) -> (id))? = objc_smart_getIMP(object: self, selector: "font")
+			if let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("font")) {
+				return NSFont(nsobjptr: rtn)
+			}
+			return nil
+		}
+		
+		set {
+			print("SETTING FONT")
+			guard let selfPtr = self._nsobjptr else {print("selfPtr = nil"); return}
+			guard let newValuePtr = newValue?._nsobjptr else {print("newValue = nil \(newValue)"); return}
+			var imp:  (@convention(c) (id, SEL, id) -> (Void))? = objc_smart_getIMP(object: self, selector: "setFont:")
+			let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("setFont:"), &newValuePtr.pointee)
+		}
+	}
 
 	public func setEditable(_ flag: ObjCBool) {
 		guard var ptr = self._nsobjptr else {return}
@@ -505,6 +686,108 @@ public class NSLabel: NSControl {
 		var bool = flag
 
 		_ = forSwift_objcSendMessage1(&ptr.pointee, sel_registerName("setSelectable:"), &bool)
+	}
+	
+	public func selectText() {
+		guard var ptr = self._nsobjptr else {return}
+		_ = forSwift_objcSendMessage(&ptr.pointee, sel_registerName("selectText"))
+	}
+	
+	public var isSelectable: Bool {
+		get {
+			var imp:  (@convention(c) (id, SEL) -> (UInt8))? = objc_smart_getIMP(object: self, selector: "isSelectable")
+			if let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("isSelectable")) {
+				return rtn == 0 ? false : true
+			}
+			return true
+		}
+		
+		set {
+			guard let selfPtr = self._nsobjptr else {return}
+			var imp:  (@convention(c) (id, SEL, UInt8) -> (Void))? = objc_smart_getIMP(object: self, selector: "setSelectable:")
+			let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("setSelectable:"), newValue ? 1 : 0)
+		}
+	}
+	
+	public var isEditable: Bool {
+		get {
+			var imp:  (@convention(c) (id, SEL) -> (UInt8))? = objc_smart_getIMP(object: self, selector: "isEditable")
+			if let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("isEditable")) {
+				return rtn == 0 ? false : true
+			}
+			return true
+		}
+		
+		set {
+			guard let selfPtr = self._nsobjptr else {return}
+			var imp:  (@convention(c) (id, SEL, UInt8) -> (Void))? = objc_smart_getIMP(object: self, selector: "setEditable:")
+			let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("setEditable:"), newValue ? 1 : 0)
+		}
+	}
+	
+	public var textColor: NSColor {
+		get {
+			var imp:  (@convention(c) (id, SEL) -> (id))? = objc_smart_getIMP(object: self, selector: "textColor")
+			if let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("textColor")) {
+				return NSColor(nsobjptr: rtn)
+			}
+			return .blue
+		}
+		
+		set {
+			guard let selfPtr = self._nsobjptr else {return}
+			guard let newValuePtr = newValue._nsobjptr else {return}
+			var imp:  (@convention(c) (id, SEL, id) -> (Void))? = objc_smart_getIMP(object: self, selector: "setTextColor:")
+			let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("setTextColor:"), &newValuePtr.pointee)
+		}
+	}
+	
+	public var isBordered: Bool {
+		get {
+			var imp:  (@convention(c) (id, SEL) -> (UInt8))? = objc_smart_getIMP(object: self, selector: "isBordered")
+			if let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("isBordered")) {
+				return rtn == 0 ? false : true
+			}
+			return true
+		}
+		
+		set {
+			guard let selfPtr = self._nsobjptr else {return}
+			var imp:  (@convention(c) (id, SEL, UInt8) -> (Void))? = objc_smart_getIMP(object: self, selector: "setBordered:")
+			let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("setBordered:"), newValue ? 1 : 0)
+		}
+	}
+	
+	public var isBezeled: Bool {
+		get {
+			var imp:  (@convention(c) (id, SEL) -> (UInt8))? = objc_smart_getIMP(object: self, selector: "isBezeled")
+			if let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("isBezeled")) {
+				return rtn == 0 ? false : true
+			}
+			return true
+		}
+		
+		set {
+			guard let selfPtr = self._nsobjptr else {return}
+			var imp:  (@convention(c) (id, SEL, UInt8) -> (Void))? = objc_smart_getIMP(object: self, selector: "setBezeled:")
+			let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("setBezeled:"), newValue ? 1 : 0)
+		}
+	}
+	
+	public var drawsBackground: Bool {
+		get {
+			var imp:  (@convention(c) (id, SEL) -> (UInt8))? = objc_smart_getIMP(object: self, selector: "drawsBackground")
+			if let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("drawsBackground")) {
+				return rtn == 0 ? false : true
+			}
+			return true
+		}
+		
+		set {
+			guard let selfPtr = self._nsobjptr else {return}
+			var imp:  (@convention(c) (id, SEL, UInt8) -> (Void))? = objc_smart_getIMP(object: self, selector: "setDrawsBackground:")
+			let rtn = imp?(&self._nsobjptr!.pointee, sel_getUid("setDrawsBackground:"), newValue ? 1 : 0)
+		}
 	}
 
 	
